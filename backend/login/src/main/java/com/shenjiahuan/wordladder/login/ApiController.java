0085548d16271e935688ea203bd6bb1eed4746ba
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-05-29 22:32:07 +0800

    added eureka

--- backend/login/src/main/java/com/shenjiahuan/wordladder/login/ApiController.java
+++ backend/login/src/main/java/com/shenjiahuan/wordladder/login/ApiController.java
@@ -1,6 +1,9 @@
 package com.shenjiahuan.wordladder.login;
 
 import com.alibaba.fastjson.JSON;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import org.codehaus.jackson.JsonNode;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +21,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    @Value("${wordladder.ip}")
-    private String wordLadderIP;
 
-    @Value("${wordladder.port}")
-    private String wordladderPort;
+    @Autowired
+    private EurekaClient eurekaClient;
 
     @RequestMapping("/wordladders")
     public Result callWordLadder(@RequestParam(value = "from", required = false) String from, @RequestParam(value = "to", required = false) String to) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://" + wordLadderIP + ":" + wordladderPort + "/wordladders";
+        Application application = eurekaClient.getApplication("wordladder-play");
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/wordladders";
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 
--- backend/play/src/main/java/com/shenjiahuan/wordladder/play/WebSecurityConfig.java
+++ backend/play/src/main/java/com/shenjiahuan/wordladder/play/WebSecurityConfig.java
@@ -17,16 +17,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Value("${gateway.ip}")
-    String gateway;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
             .authorizeRequests()
                 .antMatchers("/actuator/**").permitAll()
-                .anyRequest().hasIpAddress(gateway)
             .and()
             .csrf().
                 disable();
