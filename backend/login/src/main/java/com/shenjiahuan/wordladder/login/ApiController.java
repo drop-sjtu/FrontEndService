ff6f0dc213c61ee01202bf5506edec98edaef260
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-06-02 09:47:44 +0800

    finish hystrix

--- backend/login/src/main/java/com/shenjiahuan/wordladder/login/ApiController.java
+++ backend/login/src/main/java/com/shenjiahuan/wordladder/login/ApiController.java
@@ -1,20 +1,9 @@
 package com.shenjiahuan.wordladder.login;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
--- backend/login/src/main/java/com/shenjiahuan/wordladder/login/WordLadderController.java
+++ backend/login/src/main/java/com/shenjiahuan/wordladder/login/WordLadderController.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("wordladder-play")
+@FeignClient(value = "wordladder-play", fallback = WordLadderFallback.class)
 public interface WordLadderController {
     @RequestMapping({"/wordladders"})
     Result wordLadder(@RequestParam(value = "from", required = false) String from, @RequestParam(value = "to", required = false) String to);
