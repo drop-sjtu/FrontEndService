f21b56ac168ba49fea10c9c10040824f9727884d
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-06-28 10:28:08 +0800

    hystrix

--- backend/play/src/main/java/org/dropsjtu/play/WordLadderFeign.java
+++ backend/play/src/main/java/org/dropsjtu/play/WordLadderFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "wordladder-login")
+@FeignClient(value = "wordladder-login", fallback = WordLadderFeignFallback.class)
 public interface WordLadderFeign {
     @GetMapping(value = "/check")
     int check(@RequestParam(value = "token") String token);
